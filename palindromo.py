9316bcd9a1b60412bffd5d581806099687466001
Pelayo <phuermij@myuax.com>
2022-02-26 17:22:55 +0100

    palindromos 3

--- palindromo.py
+++ palindromo.py
@@ -1,7 +1,12 @@
 def palindromo(palabra):
+
+    #Este condicional nos devuelve True si la palabra es palíndroma y False en caso contrario
+
     if len(palabra) < 1:
         return True
     else:
+        #Este condicional va comparando la primera palabra con la última para comprobar si son iguales
+        
         if palabra[0] == palabra[-1]:
             return palindromo(palabra[1 : -1])
         else:

4d320645f56674a04ca538a9a2eaecc7d98808bf
Pelayo <phuermij@myuax.com>
2022-02-27 20:11:04 +0100

    palindromo 3

--- palindromo.py
+++ palindromo.py
@@ -1,3 +1,15 @@
+def caracteres_especiales(palabra):
+
+    palabra = palabra.lower()
+    palabra = palabra.replace(" ", "")
+    palabra = palabra.replace("á", "a")
+    palabra = palabra.replace("é", "e")
+    palabra = palabra.replace("í", "i")
+    palabra = palabra.replace("ó", "o")
+    palabra = palabra.replace("ú", "u")
+
+    return palabra
+
 def palindromo(palabra):
 
     #Este condicional nos devuelve True si la palabra es palíndroma y False en caso contrario
@@ -13,4 +25,4 @@ def palindromo(palabra):
             return False
 
 palabra = str(input("Introduce una palabra: "))
-print("La palabra ", palabra, " es palíndromo: ", palindromo(palabra))
+print("La palabra ", palabra, " es palíndromo: ", palindromo(caracteres_especiales(palabra)))
